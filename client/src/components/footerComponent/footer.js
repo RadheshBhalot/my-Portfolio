ae7f6d8df79bd22d00769ca0941a092522e63341
Radhesh Bhalot <bhalotraju949@gmail.com>
2025-02-05 13:05:31 +0530

    footer.js

--- client/src/components/footerComponent/footer.js
+++ client/src/components/footerComponent/footer.js
@@ -113,10 +113,10 @@ function Footer() {
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                 <FaSquareFacebook size={28} className='text-blue-600 hover:text-blue-800 transition duration-300' />
               </a>
-              <a href="https://in.linkedin.com/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <a href="https://in.linkedin.com/in/radhesh-bhalot-ab3b79312" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                 <FaLinkedin size={28} className='text-blue-500 hover:text-blue-700 transition duration-300' />
               </a>
-              <a href="https://www.instagram.com/?next=%2F&hl=en/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+              <a href="https://www.instagram.com/?next=%2F&hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                 <FaSquareInstagram size={28} className='text-pink-600 hover:text-pink-800 transition duration-300' />
               </a>
               <a href="https://telegram.org" target="_blank" rel="noopener noreferrer" aria-label="Telegram">
