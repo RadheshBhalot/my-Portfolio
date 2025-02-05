82f2c881e12945a8e949694b965ec72ee160cf0a
Radhesh Bhalot <bhalotraju949@gmail.com>
2025-02-05 12:36:12 +0530

    home.js

--- client/src/components/homeComponent/home.js
+++ client/src/components/homeComponent/home.js
@@ -42,8 +42,8 @@ function Home() {
                 <h3 className="font-bold text-lg md:text-xl text-gray-700">Connect with Me</h3>
                 <ul className="flex justify-center md:justify-start space-x-5 mt-4">
                   <li><a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FaSquareFacebook className="text-4xl text-blue-600 hover:text-blue-800 transition duration-300" /></a></li>
-                  <li><a href="https://in.linkedin.com/" target="_blank" rel="noreferrer"><FaLinkedin className="text-4xl text-blue-500 hover:text-blue-700 transition duration-300" /></a></li>
-                  <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><IoLogoYoutube className="text-4xl text-red-600 hover:text-red-800 transition duration-300" /></a></li>
+                 <li><a href="https://in.linkedin.com/" target="_blank" rel="noreferrer"><FaLinkedin className="text-4xl text-blue-500 hover:text-blue-700 transition duration-300" /></a></li>
+                 <li><a href="https://www.instagram.com/?next=%2F&hl=en" target="_blank" rel="noopener noreferrer"><FaSquareInstagram  className='text-4xl text-pink-600 hover:text-pink-800 transition duration-300' /></a></li>
                   <li><a href="https://web.telegram.org/a/" target="_blank" rel="noreferrer"><FaTelegram className="text-4xl text-blue-400 hover:text-blue-600 transition duration-300" /></a></li>
                 </ul>
               </div>
