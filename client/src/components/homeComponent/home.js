93af1a3a5b56e478f36937cd5fbb817bee0deaab
Radhesh Bhalot <bhalotraju949@gmail.com>
2025-02-05 15:28:24 +0530

    Update home.js

--- client/src/components/homeComponent/home.js
+++ client/src/components/homeComponent/home.js
@@ -42,8 +42,7 @@ function Home() {
                 <h3 className="font-bold text-lg md:text-xl text-gray-700">Connect with Me</h3>
                 <ul className="flex justify-center md:justify-start space-x-5 mt-4">
                   <li><a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FaSquareFacebook className="text-4xl text-blue-600 hover:text-blue-800 transition duration-300" /></a></li>
-
-                  <li><a href="https://www.instagram.com/?next=%2F&hl=en/" target="_blank" rel="noreferrer"><FaInstagram className="text-4xl text-pink-600 hover:text-pink-800 transition duration-300" /></a></li>
+                  <li><a href="https://www.instagram.com/?next=%2F&hl=en" target="_blank" rel="noreferrer"><FaInstagram className="text-4xl text-pink-600 hover:text-pink-800 transition duration-300" /></a></li>
                   <li><a href="https://www.linkedin.com/in/radhesh-bhalot-ab3b79312/" target="_blank" rel="noreferrer"><FaLinkedin className="text-4xl text-blue-500 hover:text-blue-700 transition duration-300" /></a></li>
                   <li><a href="https://web.telegram.org/a/" target="_blank" rel="noreferrer"><FaTelegram className="text-4xl text-blue-400 hover:text-blue-600 transition duration-300" /></a></li>
 
